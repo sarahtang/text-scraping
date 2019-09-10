98215773b7ed937a4c103b788da4aaca92e6465f
Sarah Tang <sarahtang@Sarahs-MacBook-Pro.local>
2019-09-09 23:07:13 -0700

    one follower df

--- twitter/twitter_followers.py
+++ twitter/twitter_followers.py
@@ -86,9 +86,7 @@ rate_headers = {
     }
 
 
-
-
-# GET RATE LIMIT
+# Get rate limit
 def get_rate_limit():
     rate_query = {"resources":"followers,users"}
     rate_response = requests.request("GET", 
@@ -106,9 +104,9 @@ def get_rate_limit():
 
 
 
-# Get list of follower ids for account = HOW TO GET ALL FOLLOWERS
 # Input url = lookup_url
 # headers = follower_headers
+# Twitter API call to get list of follower ids for an account
 def get_follower_ids(account, head, cursor):
     # Get followers query
     querystring = {"screen_name": account,
@@ -121,13 +119,10 @@ def get_follower_ids(account, head, cursor):
     return resp
 
 
-# Twitter API only can batch 100 followers at a time
-# Batch = 100 follower IDs
-def get_user_objects(follower_ids, count):
-    batch_len = 10
-    # num_batches = count / 100
-    # batches = (follower_ids[i:i + batch_len] for i in range(0, count, batch_len))
-    count = 0
+# Twitter API call to get user objects
+# ID info for list of followers
+def get_user_objects(follower_ids, count = 0):
+    # batch_len = 10
     all_data = []
     for f_id in follower_ids: #how many follower IDs
         count += 1
@@ -145,41 +140,57 @@ def get_user_objects(follower_ids, count):
     return all_data
 
 # Get info from follower json object [LIST]
-def get_follower_info_list(follower_objects):
+# WRONG
+# follower_object is a single json object with follower info // dict
+def get_follower_info_list(follower_object):
     df = []
-    for obj in follower_objects:
-        mentions_ibm = '0'
-        blank_bio = '0' #1 = blank bio
-        if "ibm" in obj['description']:
-            mentions_ibm = '1' #1 = does mention IBM
-        if "IBM" in obj['description']:
-            mentions_ibm = '1'
-        if not obj['description']:
-            blank_bio = '1'
-        df.append([
-            # obj['id'],
-            obj['screen_name'],
-            # obj['name'],
-            obj['description'],
-            # obj['verified'],
-            obj['followers_count'],
-            obj['statuses_count'],
-            # obj['url'],
-            obj['created_at'],
-            mentions_ibm,
-            blank_bio])
-    df = pd.DataFrame(df, columns=[
-                                    # 'id',
+    # print(follower_object)
+    # print(type(follower_object))
+
+    # Convert to dictionary
+    d = {}
+    for each in follower_object:
+        d['name'] = each['name']
+        d['screen_name'] = each['screen_name']
+        d['description'] = each['description']
+        d['verified'] = each['verified']
+        d['followers_count'] = each['followers_count']
+        d['statuses_count'] = each['statuses_count']
+        d['url'] = each['url']
+        d['created_at'] = each['created_at']
+        d['mentions_ibm'] = 0
+        d['blank_bio'] = 0
+        if "ibm" in d['description']:
+            d['mentions_ibm'] = 1 # 1 = mentions IBM
+        if "IBM" in d['description']:
+            d['mentions_ibm'] = 1
+        if not d['description']: # 1 = blank bio
+            d['blank_bio'] = 1
+    # print(d)
+
+    # df = pd.DataFrame(list(d.items()))
+    # df = pd.DataFrame(d, index=['i', ])
+    df.append([d['name'],
+                d['screen_name'],
+                d['description'],
+                d['verified'],
+                d['followers_count'],
+                d['statuses_count'],
+                d['url'],
+                d['created_at'],
+                d['mentions_ibm'],
+                d['blank_bio']])
+    df = pd.DataFrame(df, columns=['name',
                                     'screen_name',
-                                    # 'name',
                                     'description',
-                                    # 'verified',
+                                    'verified',
                                     'followers_count',
                                     'statuses_count',
-                                    # 'url',
+                                    'url',
                                     'created_at',
                                     'mentions_ibm',
                                     'blank_bio'])
+
     return df
 
 
@@ -203,75 +214,89 @@ def num_ids(follower_ids):
 # Can grab all followers
 # Gets all followers at a rate of 150 ids per 15 mins
 # Initialize rate limit, 150 ids per round
-rate_limit = get_rate_limit()
-cursor = -1
-account = "ibm" #ibm ==================================================== Change Account Handle
-df_ids = [] # Dataframe of follower IDs
-minute = 0
-# while (cursor != 0):
-#     if (rate_limit != 0):
-#         response = get_follower_ids(account, headers, cursor)
-#         response_json = response.json() # get list of followers per batch
-#         for ids in response_json['ids']:
-#             df_ids.append(ids)
-#         cursor = response_json['next_cursor']
-#         # print(response_json)
-#     else:
-#         print("This prints once a minute. Round:")
-#         print(minute)
-#         time.sleep(60)
-#         minute += 1
-#     rate_limit = get_rate_limit() # update rate limit
-#     print(rate_limit)
+def get_all_follower_ids(df, cursor, rate_limit, account, h): # Made this a function
+    minute = 0
+    while (cursor != 0):
+        if (rate_limit != 0):
+            response = get_follower_ids(account, h, cursor)
+            response_json = response.json() # get list of followers per batch
+            for ids in response_json['ids']:
+                df.append(ids)
+            cursor = response_json['next_cursor']
+            # print(response_json)
+        else:
+            print("This prints once a minute. Round:")
+            print(minute)
+            time.sleep(60)
+            minute += 1
+        rate_limit = get_rate_limit() # update rate limit
+        print(rate_limit)
+    return df
 
+
+# SPLIT DF INTO CHUNKS
+# Yield successive n-sized chunks of l ===============================
+def chunks(l, n):
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
+def split_into_chunks(list_follower_ids, size_of_chunk = 5000):
+    list_chunks = list(chunks(list_follower_ids, size_of_chunk))
+    print(list_chunks)
+    num = 1
+    for chunk in list_chunks:
+        print(len(chunk))
+        name = account + '_chunk' + str(num) + '.txt'
+        with open(name, "wb") as fp:
+            pickle.dump(chunk, fp)
+        num += 1
+    return list_chunks
+
+
+# Get user objects for list of followers ================================
+# Turn each chunk into CSV of follower info
+def chunk_to_follower_csv(account, chunk_name):
+    with open(chunk_name + '.txt', "rb") as fp:
+        loaded = pickle.load(fp)
+    print(loaded)
+    follower_data = get_user_objects(loaded, 0)
+    # print("FOLLOWER DATA")
+    # print(follower_data)
+    df_chunk = []
+    df_chunk = get_follower_info_list(follower_data)
+    print(df_chunk)
+    df_chunk.to_csv('twitter_followers_' + chunk_name + 'TEST.csv', index=False, encoding='utf8')
+    # to_excel('.xls')
+    return df_chunk
+
+
+
+
+
+
+# FUNCTION CALLS ===============================================================================
+# ==============================================================================================
+
+df_ids = [] # Dataframe of ALL follower IDs
+initial_rate_limit = get_rate_limit()
+account = "sarahmtang" #ibm ==================================================== Change Account Handle
+
+# # Call to get all followerids
+# get_all_follower_ids(df_ids, -1, initial_rate_limit, account, headers) # Get follower objects
 
 # df_ids = pd.DataFrame(df_ids, columns = ['follower_id']) #This has correct number and IDs ==> use this in get_user_objects
 # print('number of dataframe ids')
 # print(df_ids)
 
-# Analyzing followers
-# List of all ids
+# # List of all ids
 # list_follower_ids = df_ids['follower_id'].tolist() # ===================
-# print('len of follower id list length')
-# print(len(list_follower_ids))
-# print(list_follower_ids) # prints a list of all follower ids
 
+# # Split  df into chunks
+# split_into_chunks(list_follower_ids, 5)
 
-# TO DO split up df_ids
-# IBMStorage - split into 3
-# Yield successive n-sized chunks of l ===============================
-# def chunks(l, n):
-#     for i in range(0, len(l), n):
-#         yield l[i:i + n]
-
-# list_chunks = list(chunks(list_follower_ids, 5000))
-# print(list_chunks)
-# num = 1
-# for chunk in list_chunks:
-#     print(len(chunk))
-#     name = account + '_chunk' + str(num) + '.txt'
-#     with open(name, "wb") as fp:
-#         pickle.dump(chunk, fp)
-#     num += 1
-
-
-# Get user objects for list of followers ================================
-
-with open("IBM_chunk13.txt", "rb") as fp:
-    loaded = pickle.load(fp)
-
-print(loaded)
-
-follower_data = get_user_objects(loaded, 0)
-# print(follower_data) # print JSON objects of each follower
-
-df = []
-df = get_follower_info_list(follower_data)
-print(df) #98 rows right now, missing the first and the last
-
-df.to_csv('twitter_followers_' + account + '13.csv', index=False, encoding='utf8')
-# to_excel('.xls')
-
+# Chunks to csv
+chunk_name = "sarahmtang_chunk1"
+chunk_to_follower_csv(account, chunk_name)
 
 
 
